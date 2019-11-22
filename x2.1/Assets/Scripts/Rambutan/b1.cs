9707022e95ae5195f109dc8cab4c55db6246ec7b
Student <student@mirea.ru>
2019-11-22 10:29:43 +0300

    added prj

---
+++ x2.1/Assets/Scripts/Rambutan/b1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b1 : MonoBehaviour
+{
+    public float c;
+    static float l = 0.3f;
+
+
+    [SerializeField] private GameObject cylinder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float dc1 = c * l / 100f;
+        float dc = l - dc1;
+        float s = dc / 8f;
+        cylinder.transform.localScale -= new Vector3(0, dc, 0);
+        cylinder.transform.position -= new Vector3(0, 0, s);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
