164a5348fcdbf04651d0ff50da7a37e6c1317e73
dima <d.sap2003@yandex.ru>
2019-12-13 11:39:01 +0300

    button

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public ParticleSystem Lightning;
+
+    private void OnMouseDown()
+    {
+        Lightning.Stop();
+        var vec = gameObject.transform.position - Lightning.transform.position;
+        var distance = vec.magnitude;
+        Lightning.GetComponent<ParticleSystemRenderer>().lengthScale = distance;
+        Lightning.transform.LookAt(gameObject.transform);
+        Lightning.transform.Rotate(new Vector3(0, 180, 0));
+        Lightning.Play();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
